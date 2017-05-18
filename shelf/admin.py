3160e1e74d0e8d8e88693da49d9f6d6a21ae1cfa
gulci-poz <sebastian.gulczynski.poznan@gmail.com>
2017-05-18 14:40:41 +0200

    2017-05-18 some displaying options in admin app

--- shelf/admin.py
+++ shelf/admin.py
@@ -2,4 +2,17 @@ from django.contrib import admin
 
 from .models import Author, Book, Publisher
 
-admin.site.register([Author, Book, Publisher])
+
+class AuthorAdmin(admin.ModelAdmin):
+    search_fields = ['last_name', 'first_name']
+    # sortowanie w malejącym porządku: '-last_name'
+    ordering = ['last_name']
+
+
+class BookAdmin(admin.ModelAdmin):
+    search_fields = ['title']
+    list_display = ['title', 'author', 'isbn', 'publisher']
+
+admin.site.register(Author, AuthorAdmin)
+admin.site.register(Book, BookAdmin)
+admin.site.register([Publisher])
