e360a73257b657f4c0b5e753d80d92467b4cdce5
gulci-poz <sebastian.gulczynski.poznan@gmail.com>
2017-05-19 10:14:25 +0200

    2017-05-19 basic list and details views of authors, end of session no 3

---
+++ shelf/urls.py
@@ -0,0 +1,16 @@
+from django.conf.urls import url
+
+from shelf.views import AuthorDetailView, AuthorListView
+
+# alternatywnie do namespace w include() w nadrzędnym pliku urls.py
+# app_name = 'shelf'
+
+urlpatterns = [
+    url(r'^authors/$',
+        AuthorListView.as_view(),
+        name='author-list'),
+
+    url(r'^authors/(?P<pk>\d+)/$',
+        AuthorDetailView.as_view(),
+        name='author-detail'),
+]
