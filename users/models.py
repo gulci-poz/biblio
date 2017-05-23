754b27a2c64e0027ae2f804ac902b30f3fde6efd
gulci-poz <sebastian.gulczynski.poznan@gmail.com>
2017-05-23 13:27:24 +0200

    2017-05-23 admin form for custom user using fieldsets, still in sesssion no 8

--- users/models.py
+++ users/models.py
@@ -3,5 +3,5 @@ from django.db import models
 
 
 class BiblioUser(AbstractUser):
-    avatar = models.ImageField(upload_to='avatars/')
-    phone = models.CharField(max_length=15)
+    avatar = models.ImageField(upload_to='avatars/', blank=True, null=True)
+    phone = models.CharField(max_length=15, blank=True, default='')
